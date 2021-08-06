01a04c056ad4934be9a398ef4ececdef39961359
Pradyumna Krishna <51270439+PradyumnaKrishna@users.noreply.github.com>
2021-08-06 21:39:16 +0530

    user management feature (login/logout/signup)

--- server/app/urls.py
+++ server/app/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name="about"),
     path('contact', views.contact, name="about"),
+
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('registration/', views.signup, name='signup'),
 ]
--- server/app/views.py
+++ server/app/views.py
@@ -1,13 +1,74 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.http import Http404
+from django.shortcuts import render, redirect
 
 
 def index(request):
+    """ index view """
     return render(request, template_name="app/index.html")
 
 
 def about(request):
+    """ contact view """
     return render(request, template_name="app/about.html")
 
 
 def contact(request):
+    """ contact view """
     return render(request, template_name="app/contact.html")
+
+
+def signup(request):
+    """ user signup view """
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'app/user_signup.html', context)
+
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        existing_user = User.objects.filter(username=username).first()
+
+        if not existing_user:
+            user = User.objects.create_user(
+                username=username,
+                first_name=first_name,
+                last_name=last_name,
+                password=password
+            )
+
+            login(request, user)
+            return redirect("app:index")
+
+        context['message'] = "User already exists."
+        return render(request, 'app/user_signup.html', context)
+
+    return Http404
+
+
+def login_view(request):
+    """ user login view """
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('app:index')
+
+        context['message'] = "Invalid username or password."
+        return render(request, 'app/user_login.html', context)
+
+    return render(request, 'app/user_login.html', context)
+
+
+def logout_view(request):
+    """ user logout view """
+    logout(request)
+    return redirect('app:index')
